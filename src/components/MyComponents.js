79c5d7a352c364297adcc171a059445cde6842c4
Van Dao <dao28901@gmail.com>
2024-10-02 10:09:42 +0700

    Event

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -5,10 +5,16 @@ class MyComponents extends React.Component {
     address: "Nam Dinh",
     age: 19,
   };
+  handleClick(event) {
+    console.log(event.pageX);
+    console.log(this.state.name);
+  }
   render() {
     return (
       <div>
         my name is {this.state.name} and im from {this.state.address}
+        <br />
+        <button onMouseOver={this.handleClick}>push</button>
       </div>
     );
   }
