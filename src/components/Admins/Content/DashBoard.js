01b09fb242bb9ee7f8ba9471e71927d1cfb5436c
Van Dao <dao28901@gmail.com>
2024-12-14 18:45:35 +0700

    #111. Tích hợp api dashboard

--- src/components/Admins/Content/DashBoard.js
+++ src/components/Admins/Content/DashBoard.js
@@ -7,48 +7,110 @@ import {
   XAxis,
   YAxis,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
 import "./DashBoard.scss";
+import { getOverView } from "../../../service/apiService";
+import { useEffect, useState } from "react";
+import { queryByTestId } from "@testing-library/react";
 // import { Tooltip } from "bootstrap";
 
 const DashBoard = (props) => {
-  const data = [
-    {
-      name: "A",
-      uv: 4000,
-      pv: 2400,
-    },
-    {
-      name: "B",
-      uv: 6000,
-      pv: 4800,
-    },
-    {
-      name: "C",
-      uv: 9000,
-      pv: 5600,
-    },
-  ];
+  const [dataOverview, setDataOverview] = useState([]);
+  const [dataChart, setDataChart] = useState([]);
+  useEffect(() => {
+    fetchDataOverview();
+  }, []);
+  const fetchDataOverview = async () => {
+    let res = await getOverView();
+    if (res && res.EC === 0) {
+      setDataOverview(res.DT);
+      let Qz,
+        Qs,
+        As = 0;
+      Qz = res?.DT?.others?.countQuiz ?? 0;
+      Qs = res?.DT?.others?.countQuestions ?? 0;
+      As = res?.DT?.others?.countAnswers ?? 0;
+      const data = [
+        {
+          name: "Quizzes",
+          Qz: Qz,
+        },
+        {
+          name: "Questions",
+          Qs: Qs,
+        },
+        {
+          name: "Answers",
+          As: As,
+        },
+      ];
+      setDataChart(data);
+    }
+    console.log(">>>", res);
+  };
+
   return (
     <div className="dashboard-container">
-      <div className="title">DashBoard</div>
+      <div className="title my-2">DashBoard</div>
       <div className="content">
         <div className="c-left">
-          <div className="child">Total users</div>
-          <div className="child">Total quizzes</div>
-          <div className="child">Total questions</div>
-          <div className="child">Total answers</div>
+          <div className="child">
+            <span>Total users</span>
+            <span className="text-2">
+              {dataOverview &&
+              dataOverview.users &&
+              dataOverview.users.total ? (
+                <>{dataOverview.users.countUsers}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span>Total quizzes</span>
+            <span className="text-2">
+              {dataOverview && dataOverview.others ? (
+                <>{dataOverview.others.countQuiz}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span>Total questions</span>
+            <span className="text-2">
+              {dataOverview && dataOverview.others ? (
+                <>{dataOverview.others.countQuestions}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span>Total answers</span>
+            <span className="text-2">
+              {dataOverview && dataOverview.others ? (
+                <>{dataOverview.others.countAnswers}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
         </div>
         <div className="c-right">
-          <BarChart width={700} height={250} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey={"name"} />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey={"pv"} fill="blue" />
-            <Bar dataKey={"uv"} fill="red" />
-          </BarChart>
+          <ResponsiveContainer width="95%" height="100%">
+            <BarChart data={dataChart}>
+              <CartesianGrid />
+              <XAxis dataKey={"name"} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey={"Qz"} fill="blue" />
+              <Bar dataKey={"Qs"} fill="red" />
+              <Bar dataKey={"As"} fill="yellow" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
--- src/service/apiService.js
+++ src/service/apiService.js
@@ -105,6 +105,9 @@ const postUpsertQA = (data) => {
 const logout = (email, refresh_token) => {
   return axios.post(`/api/v1/logout`, { email, refresh_token });
 };
+const getOverView = () => {
+  return axios.get(`api/v1/overview`);
+};
 export {
   postCreateNewUser,
   getAllUserService,
@@ -126,4 +129,5 @@ export {
   getQuizWithQA,
   postUpsertQA,
   logout,
+  getOverView,
 };
